b28e9023dbf8cf374c8e9ffd107e77f5e22c844f
d00002 <1356628175@qq.com>
2019-12-13 19:34:31 +0800

    dong

--- attr_goods.sql
+++ attr_goods.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50553
 File Encoding         : 65001
 
-Date: 2019-12-09 14:16:24
+Date: 2019-12-13 19:30:33
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -22,63 +22,70 @@ DROP TABLE IF EXISTS `attr_goods`;
 CREATE TABLE `attr_goods` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
-  `class` varchar(255) DEFAULT NULL,
-  `status` varchar(255) DEFAULT NULL,
   `pid` int(10) DEFAULT NULL,
   `cid` int(10) DEFAULT NULL,
   `brand_id` int(10) DEFAULT NULL,
   `sku` text,
+  `class_id` int(11) DEFAULT NULL,
+  `depict` varchar(255) DEFAULT NULL,
+  `shou` varchar(255) DEFAULT NULL,
+  `hua` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `shang` varchar(255) DEFAULT NULL,
+  `count` int(11) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `worehouse` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=57 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=61 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of attr_goods
 -- ----------------------------
-INSERT INTO `attr_goods` VALUES ('50', '奶茶', null, null, null, '1', null, null);
-INSERT INTO `attr_goods` VALUES ('2', '衣服', null, null, '0', null, '1', null);
-INSERT INTO `attr_goods` VALUES ('3', '化妆品', null, null, '0', null, '1', null);
-INSERT INTO `attr_goods` VALUES ('10', '所属', null, null, null, null, '2', null);
-INSERT INTO `attr_goods` VALUES ('5', '是', null, null, '2', null, '2', null);
-INSERT INTO `attr_goods` VALUES ('6', '解决', null, null, '3', null, '3', null);
-INSERT INTO `attr_goods` VALUES ('8', null, null, null, null, null, '3', null);
-INSERT INTO `attr_goods` VALUES ('9', '哈哈', null, null, null, null, null, null);
-INSERT INTO `attr_goods` VALUES ('11', '很好听', null, null, '2', null, null, null);
-INSERT INTO `attr_goods` VALUES ('12', '很好听', null, null, '2', null, null, null);
-INSERT INTO `attr_goods` VALUES ('13', '很好听', null, null, '2', null, null, null);
-INSERT INTO `attr_goods` VALUES ('14', '对对', null, null, '1', null, null, null);
-INSERT INTO `attr_goods` VALUES ('15', '可乐', null, null, null, null, null, null);
-INSERT INTO `attr_goods` VALUES ('16', '嘻哈', null, null, null, '2', null, null);
-INSERT INTO `attr_goods` VALUES ('17', '雪碧', null, null, null, '1', null, null);
-INSERT INTO `attr_goods` VALUES ('18', '雪碧', null, null, null, '1', null, null);
-INSERT INTO `attr_goods` VALUES ('19', '奶', null, null, null, '1', null, null);
-INSERT INTO `attr_goods` VALUES ('20', '奶', null, null, null, '1', null, null);
-INSERT INTO `attr_goods` VALUES ('21', '奶', null, null, null, '1', null, null);
-INSERT INTO `attr_goods` VALUES ('22', '奶', null, null, null, '1', null, null);
-INSERT INTO `attr_goods` VALUES ('23', '网', null, null, null, '4', null, null);
-INSERT INTO `attr_goods` VALUES ('24', '电脑', null, null, null, '3', null, null);
-INSERT INTO `attr_goods` VALUES ('25', '电脑', null, null, null, '3', null, null);
-INSERT INTO `attr_goods` VALUES ('26', '电脑', null, null, null, '3', null, null);
-INSERT INTO `attr_goods` VALUES ('27', '电脑', null, null, null, '3', null, null);
-INSERT INTO `attr_goods` VALUES ('28', '东鹏', null, null, null, '1', null, null);
-INSERT INTO `attr_goods` VALUES ('29', '东鹏', null, null, null, '1', null, null);
-INSERT INTO `attr_goods` VALUES ('30', '东鹏', null, null, null, '1', null, null);
-INSERT INTO `attr_goods` VALUES ('31', '奶茶', null, null, null, '1', null, null);
-INSERT INTO `attr_goods` VALUES ('32', '试试', null, null, null, '2', null, null);
-INSERT INTO `attr_goods` VALUES ('33', '烤鱼', null, null, null, '4', null, null);
-INSERT INTO `attr_goods` VALUES ('34', '哇哈哈', null, null, null, '1', null, null);
-INSERT INTO `attr_goods` VALUES ('35', '咔嚓裤子', null, null, null, '2', null, null);
-INSERT INTO `attr_goods` VALUES ('36', '豆浆', null, null, null, '1', null, null);
-INSERT INTO `attr_goods` VALUES ('37', '豆浆', null, null, null, '1', null, null);
-INSERT INTO `attr_goods` VALUES ('38', 'ss', null, null, null, '1', null, null);
-INSERT INTO `attr_goods` VALUES ('39', '哈哈', null, null, null, null, null, null);
-INSERT INTO `attr_goods` VALUES ('40', '哇哈哈', null, null, null, '1', null, null);
-INSERT INTO `attr_goods` VALUES ('41', '哇哈哈', null, null, null, '1', null, null);
-INSERT INTO `attr_goods` VALUES ('42', '哇哈哈', null, null, null, null, null, null);
-INSERT INTO `attr_goods` VALUES ('43', '哇哈哈', null, null, null, null, null, null);
-INSERT INTO `attr_goods` VALUES ('44', '哇哈哈', null, null, null, null, null, null);
-INSERT INTO `attr_goods` VALUES ('45', '哇哈哈', null, null, null, null, null, null);
-INSERT INTO `attr_goods` VALUES ('46', '哇哈哈', null, null, null, null, null, null);
-INSERT INTO `attr_goods` VALUES ('47', '哇哈哈', null, null, null, null, null, null);
-INSERT INTO `attr_goods` VALUES ('48', '哇哈哈', null, null, null, null, null, null);
-INSERT INTO `attr_goods` VALUES ('49', '测试1', null, null, null, null, null, null);
-INSERT INTO `attr_goods` VALUES ('56', null, null, null, null, null, null, '{\"name\":\"\\u8bd5\\u8bd5\",\"id\":[\"\\u9ed1\\u8272\",\"l\",\"1k\"]}');
+INSERT INTO `attr_goods` VALUES ('59', '哇哈哈哈', null, '1', null, null, null, null, null, null, null, null, null, null, '南京仓库');
+INSERT INTO `attr_goods` VALUES ('3', '化妆品', '0', null, '1', null, null, null, null, null, null, null, null, null, '河北仓库');
+INSERT INTO `attr_goods` VALUES ('10', '所属', null, null, '2', null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('5', '是', '2', null, '2', null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('6', '解决', '3', null, '3', null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('8', null, null, null, '3', null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('9', '哈哈', null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('11', '很好听', '2', null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('12', '很好听', '2', null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('13', '很好听', '2', null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('14', '对对', '1', null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('15', '可乐', '1', null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('16', '嘻哈', null, '2', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('17', '雪碧', null, '1', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('18', '雪碧', null, '1', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('19', '奶', null, '1', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('20', '奶', null, '1', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('21', '奶', null, '1', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('22', '奶', null, '1', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('58', '冲电头', null, '3', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('25', '电脑', null, '3', null, null, null, null, null, null, null, null, null, null, '北京仓库');
+INSERT INTO `attr_goods` VALUES ('26', '电脑', null, '3', null, null, null, null, null, null, null, null, null, null, '上海仓库');
+INSERT INTO `attr_goods` VALUES ('27', '电脑', null, '3', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('28', '东鹏', null, '1', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('29', '东鹏', null, '1', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('30', '东鹏', null, '1', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('31', '奶茶', null, '1', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('32', '试试', null, '2', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('33', '烤鱼', null, '4', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('34', '哇哈哈', null, '1', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('35', '咔嚓裤子', null, '2', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('36', '豆浆', null, '1', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('37', '豆浆', null, '1', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('38', 'ss', null, '1', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('39', '哈哈', null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('40', '哇哈哈', null, '1', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('41', '哇哈哈', null, '1', null, null, '2', null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('42', '哈伦裤子', null, null, null, null, '2', null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('43', '哈哈裤子', null, null, null, null, '2', null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('44', '哇哈哈', null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('45', '哇哈哈', null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('46', '哇哈哈', null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('47', '哇哈哈', null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('48', '哇哈哈', null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('49', '测试1', null, null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('57', 'iphone', null, '3', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('56', null, null, null, null, '{\"name\":\"\\u8bd5\\u8bd5\",\"id\":[\"\\u9ed1\\u8272\",\"l\",\"1k\"]}', null, null, null, null, null, null, null, null, null);
+INSERT INTO `attr_goods` VALUES ('60', '充电宝', null, '3', null, null, null, null, null, null, null, null, null, null, null);
